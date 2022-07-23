0e4d02055788ef68733f9791c9c910ff1f50cb20
LazyKernel <miksu.fredriksson@hotmail.com>
2022-07-23 16:11:57 +0300

    Finish frequency data wrangling

--- src/freqs.py
+++ src/freqs.py
@@ -1,19 +1,12 @@
 import os.path
 import pandas as pd
-import numpy as np
-from anki.Anki import Anki
-from anki.Format import Format
-from data_sources.Forvo import Forvo
 from data_sources.DBUtil import DBUtil
-from data_sources.General import General
-from data_sources.Reibun import Reibun
 from data_sources.JPDB import JPDB
 
 
 def construct_kanji_list():
-    con = DBUtil.get_con()
-    
-    if not os.path.isfile('temp_freq.csv'):
+    if not os.path.isfile('source/temp_freq.csv'):
+        con = DBUtil.get_con()
         cur = con.cursor()
         cur.execute(
             '''
@@ -42,12 +35,30 @@ def construct_kanji_list():
             con=con
         )
         print(df)
-        # this sql takes ages to run (a couple of minutes)
-        # so caching the result to avoid heating my room unnecessarily
-        df.to_csv('temp_freq.csv', index=False, encoding='utf-8')
+        # this sql takes ages to run (hours) so caching the result
+        df.to_csv('source/temp_freq.csv', index=False, encoding='utf-8')
 
-    # closing the connection to get rid of the temporary table
-    DBUtil.close_con()
+        # closing the connection to get rid of the temporary table
+        DBUtil.close_con()
+    else:
+        # read from cache
+        df = pd.read_csv('source/temp_freq.csv', encoding='utf-8')
+
+    # first sort the words within groups
+    df = df.groupby(['kanji', 'kanji_freq']).apply(lambda x: x.sort_values('freq'))
+    # reset index to get back flat df
+    df = df.reset_index(drop=True)
+    # save the order within groups for pivot
+    df['order'] = df.groupby(['kanji', 'kanji_freq']).cumcount() + 1
+    # pivot the table to have 5 columns for each of the words + freqs
+    df = df.pivot(index=['kanji', 'kanji_freq'], columns=['order'], values=['word', 'freq'])
+    # reset index and fix columns to remove multi index for both
+    df = df.reset_index()
+    df.columns = [''.join([str(c) for c in col]).strip() for col in df.columns.values]
+    # finally sort the rows by kanji frequency
+    df = df.sort_values('kanji_freq').reset_index(drop=True)
+    
+    df.to_csv('out/kanjis_with_words_by_freq.csv', index=False, encoding='utf-8')
 
 
 def fetch():
